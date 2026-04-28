cd243de22cc789bc4a8bb3beb88f1f86d9c9b422
minh <hadtnt71@gmail.com>
2026-04-28 09:32:06 +0700

    Re inforcement heap structure with max and min heap

--- algorithm + contest/pattern/dp.py
+++ algorithm + contest/pattern/dp.py
@@ -26,12 +26,34 @@ print(sol.minPathSum([[1, 3, 1], [1, 5, 1], [4, 2, 1]]))
 class WordBreak:
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
         n = len(s)
-        dp= [False] * (n+1)
+        dp = [False] * (n+1)
         dp[0] = True
         for i in range(1, n+1):
             for word in wordDict:
                 if i >= len(word) and dp[i - len(word)] and s[i - len(word): i] == word:
                     dp[i] = True
                     break
-        
+
         return dp[n]
+
+
+class SolvingQuestionWithBrainPower:
+    # dp[i] most points can receive when solve questions from i -> n
+    def mostPoints(self, questions: List[List[int]]) -> int:
+        n = len(questions)
+        dp = [0] * (n+1)  # space for the skip of n-1
+        for i in range(n-1, -1, -1):
+            point, brainpower = questions[i]
+
+            total_solved = point
+
+            next_question = i + brainpower + 1
+            if next_question < n:
+                total_solved += dp[next_question]
+
+            dp[i] = max(total_solved, dp[i+1])
+        return dp[0]
+
+
+sol = SolvingQuestionWithBrainPower()
+print(sol.mostPoints([[3, 2], [4, 3], [4, 4], [2, 5]]))
--- algorithm + contest/pattern/heap.py
+++ algorithm + contest/pattern/heap.py
@@ -1,18 +1,31 @@
 import heapq
+from typing import List
 
 
 class KthLargest:
 
     def __init__(self, k: int, nums: List[int]):
         self.k = k
-        self.scores = []  # min_heap
-        for num in nums:
-            self.add(num)
+        self.pq = nums
+
+        heapq.heapify(self.pq)
 
     def add(self, val: int) -> int:
-        heapq.heappush(self.scores, val)
+        heapq.heappush(self.pq, val)
+        while len(self.pq) > self.k:
+            heapq.heappop(self.pq)
+        return self.pq[0]
 
-        if len(self.scores) > self.k:
-            heapq.heappop(self.scores)
 
-        return self.scores[0]
+sol = KthLargest(3, [4, 5, 8, 2])
+
+sol.add(3)
+print(sol.pq)
+sol.add(5)
+print(sol.pq)
+sol.add(10)
+print(sol.pq)
+sol.add(9)
+print(sol.pq)
+sol.add(4)
+print(sol.pq)
