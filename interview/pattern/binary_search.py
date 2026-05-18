54f0331e47c210f99877482dabc496f0f8251f53
minh <hadtnt71@gmail.com>
2026-05-18 13:10:43 +0700

    Update BS pattern

--- interview/pattern/binary_search.py
+++ interview/pattern/binary_search.py
@@ -99,3 +99,37 @@ print(sol.maxDistance(side=2, points=[[0,0],[1,2],[2,0], [2,2], [2,1]], k=4))
 print(sol.maxDistance(side=4, points=[
       [4, 4], [3, 4], [2, 0], [4, 3], [4, 0]], k=4))
 print(sol.maxDistance(side=15, points=[[0,11],[15,15],[0,0],[0,8],[14,0]], k=4))
+
+class Solution:
+    def countKthRoots(self, l: int, r: int, k: int) -> int:
+        def bound_left(): # ceil(roots_k(l))
+            left, right = 0,int(l**(1/k)) + 1 if l > 0 else 0
+            while left < right:
+                mid = left + (right-left) // 2
+                perf = mid**k
+                if perf == l:
+                    return mid
+                if perf < l:
+                    left = mid + 1
+                else:
+                    right = mid
+            return left
+        def bound_right(): # floor(roos_k(r))
+            left, right = 0, int(r**(1/k)) + 1 if r > 0 else 0
+            while left <= right:
+                mid = left + (right-left) // 2
+                perf = mid**k
+                if perf == r:
+                    return mid
+                if perf > r:
+                    right = mid - 1
+                else:
+                    left = mid + 1
+            return left - 1
+        return bound_right() - bound_left() + 1
+
+sol = Solution()
+print(sol.countKthRoots(1,9,3))
+print(sol.countKthRoots(8,30,2))
+print(sol.countKthRoots(19,22,1))
+print(sol.countKthRoots(2,3,2))
--- interview/pattern/think.py
+++ interview/pattern/think.py
@@ -1,15 +1,33 @@
 class Solution:
-    def isAdjacentDiffAtMostTwo(self, s: str) -> bool:
-        if len(s) <= 1:
-            return True
-        n = len(s)
-        prev = ord(s[0])
-        for i in range(1,n):
-            curr = ord(s[i])
-            if abs(curr-prev) > 2:
-                return False
-            prev = curr
-        return True
+    def countKthRoots(self, l: int, r: int, k: int) -> int:
+        def bound_left(): # ceil(roots_k(l))
+            left, right = 0,int(l**(1/k)) + 1 if l > 0 else 0
+            while left < right:
+                mid = left + (right-left) // 2
+                perf = mid**k
+                if perf == l:
+                    return mid
+                if perf < l:
+                    left = mid + 1
+                else:
+                    right = mid
+            return left
+        def bound_right(): # floor(roos_k(r))
+            left, right = 0, int(r**(1/k)) + 1 if r > 0 else 0
+            while left <= right:
+                mid = left + (right-left) // 2
+                perf = mid**k
+                if perf == r:
+                    return mid
+                if perf > r:
+                    right = mid - 1
+                else:
+                    left = mid + 1
+            return left - 1
+        return bound_right() - bound_left() + 1
 
 sol = Solution()
-print(sol.isAdjacentDiffAtMostTwo("132"))
+print(sol.countKthRoots(1,9,3))
+print(sol.countKthRoots(8,30,2))
+print(sol.countKthRoots(19,22,1))
+print(sol.countKthRoots(2,3,2))
